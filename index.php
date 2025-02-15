5d9fac98c10c9eaadc93441ec830d094f081961c
ipel <gustiwaty78@gmail.com>
2025-02-15 21:47:04 +0700

    menambahkan tombol

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus tenetur iure exercitationem, aliquam ad alias eaque asperiores dicta ipsam dignissimos? Id illum repudiandae distinctio necessitatibus quod laborum quas nam. Nobis.</p>
+    <button>Submit</button>
 </body>
 </html>
